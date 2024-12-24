2348f2028d2d537afbb1dcb4d4ebea0690816440
jimmyyy <jimmy1141450313@gmail.com>
2024-12-24 16:06:34 +0800

    change default page

--- bot.js
+++ bot.js
@@ -43,13 +43,14 @@ bot.command("start", async (ctx) => {
             inline_keyboard: [
                 [{ text: "Start", callback_data: "start_conversation" }],
                 [{ text: "Open Web App", web_app: { url: "https://test2-git-main-answerball9527s-projects.vercel.app/" } }],
-                [{ text: "Open Web App1", web_app: { url: "https://next-wagmi-template.vercel.app/" } }],
+                [{ text: "Open Web App1", web_app: { url: "https://next-wagmi-template.vercel.app/login" } }],
                 [{ text: "Open stakestone", web_app: { url: "https://stakestone.io/" } }],
                 [{ text: "Share with others", switch_inline_query: "Check this out!" }]
             ]
         }
     });
 });
+
 
 bot.command("ping", async (ctx) => {
     await ctx.reply("Pong!");
